f9633e466974d1be63b51ac780e1e2733c15999d
AlexKnauth <alexander@knauth.org>
2022-06-15 01:32:00 -0400

    Update match-dots tests for 'match-ellipsis-depth

--- typed-racket-test/succeed/match-dots.rkt
+++ typed-racket-test/succeed/match-dots.rkt
@@ -5,4 +5,6 @@
 (: parse-sexpr : Any -> Number)
 (define (parse-sexpr sexpr)
   (match sexpr
-    [(list #{(? symbol? a) : (Listof Symbol)} ...) 1]))
+    [(list (? symbol? #{a : Symbol}) ...)
+     (ann a (Listof Symbol))
+     1]))
--- typed-racket-test/succeed/match-dots2.rkt
+++ typed-racket-test/succeed/match-dots2.rkt
@@ -6,4 +6,6 @@
 ;; evaluates a postfix sequence of items, using a stack
 (define (post-eval)
   (match '(1 2)
-    [(list (? number? #{stack : (Listof Number)}) ...) 3]))
+    [(list (? number? #{stack : Number}) ...)
+     (ann stack (Listof Number))
+     3]))
